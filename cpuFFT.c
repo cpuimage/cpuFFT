d02877a2673db7ae8cc7965f16f5463b10c27653
cpuimage <gaozhihan@vip.qq.com>
2018-08-18 20:09:35 +0800

    make code reading easy

--- cpuFFT.c
+++ cpuFFT.c
@@ -21,24 +21,24 @@ cmplx cmplx_mul_add(const cmplx c, const cmplx a, const cmplx b) {
     return ret;
 }
 
-void fft_Stockham(const cmplx *input, cmplx *output, size_t n, int flag) {
-    size_t half = n >> 1;
+void fft_Stockham(const cmplx *input, cmplx *output, int n, int flag) {
+    int half = n >> 1;
     cmplx *buffer = (cmplx *) calloc(sizeof(cmplx), n * 2);
     if (buffer == NULL)
         return;
     cmplx *tmp = buffer;
     cmplx *y = tmp + n;
     memcpy(y, input, sizeof(cmplx) * n);
-    for (size_t r = half, l = 1; r >= 1; r >>= 1) {
+    for (int r = half, l = 1; r >= 1; r >>= 1) {
         cmplx *tp = y;
         y = tmp;
         tmp = tp;
         float factor_w = -flag * M_PI / l;
         cmplx w = {cosf(factor_w), sinf(factor_w)};
         cmplx wj = {1, 0};
-        for (size_t j = 0; j < l; j++) {
-            size_t jrs = j * (r << 1);
-            for (size_t k = jrs, m = jrs >> 1; k < jrs + r; k++) {
+        for (int j = 0; j < l; j++) {
+            int jrs = j * (r << 1);
+            for (int k = jrs, m = jrs >> 1; k < jrs + r; k++) {
                 const cmplx t = {(wj.real * tmp[k + r].real) - (wj.imag * tmp[k + r].imag),
                                  (wj.imag * tmp[k + r].real) + (wj.real * tmp[k + r].imag)};
                 y[m].real = tmp[k].real + t.real;
@@ -57,37 +57,37 @@ void fft_Stockham(const cmplx *input, cmplx *output, size_t n, int flag) {
     free(buffer);
 }
 
-void fft_radix3(const cmplx *input, cmplx *output, size_t n, int flag) {
+void fft_radix3(const cmplx *input, cmplx *output, int n, int flag) {
     if (n < 2) {
         output[0] = input[0];
         return;
     }
-    size_t radix = 3;
-    size_t np = n / radix;
+    int radix = 3;
+    int np = n / radix;
     cmplx *res = (cmplx *) malloc(sizeof(cmplx) * n);
     cmplx *f0 = res;
     cmplx *f1 = f0 + np;
     cmplx *f2 = f1 + np;
-    for (size_t i = 0; i < np; i++) {
-        for (size_t j = 0; j < radix; j++) {
+    for (int i = 0; i < np; i++) {
+        for (int j = 0; j < radix; j++) {
             res[i + j * np] = input[radix * i + j];
         }
     }
     fft_radix3(f0, f0, np, flag);
     fft_radix3(f1, f1, np, flag);
     fft_radix3(f2, f2, np, flag);
-    float wexp0 = -2 * (float) M_PI * (flag) / (float) (n);
+    float wexp0 = -2 * M_PI * flag / n;
     cmplx wt = {cosf(wexp0), sinf(wexp0)};
     cmplx w0 = {1, 0};
-    for (size_t i = 0; i < np; i++) {
+    for (int i = 0; i < np; i++) {
         const float w0r = w0.real;
         w0.real = (w0r * wt.real) - (w0.imag * wt.imag);
         w0.imag = (w0.imag * wt.real) + (w0r * wt.imag);
     }
     cmplx w = {1, 0};
-    for (size_t j = 0; j < radix; j++) {
+    for (int j = 0; j < radix; j++) {
         cmplx wj = w;
-        for (size_t k = 0; k < np; k++) {
+        for (int k = 0; k < np; k++) {
             output[k + j * np] = cmplx_mul_add(f0[k], cmplx_mul_add(f1[k], f2[k], wj), wj);
             const float wjr = wj.real;
             wj.real = (wjr * wt.real) - (wj.imag * wt.imag);
@@ -100,21 +100,21 @@ void fft_radix3(const cmplx *input, cmplx *output, size_t n, int flag) {
     free(res);
 }
 
-void fft_radix5(const cmplx *input, cmplx *output, size_t n, int flag) {
+void fft_radix5(const cmplx *input, cmplx *output, int n, int flag) {
     if (n < 2) {
         output[0] = input[0];
         return;
     }
-    size_t radix = 5;
-    size_t np = n / radix;
+    int radix = 5;
+    int np = n / radix;
     cmplx *res = (cmplx *) calloc(sizeof(cmplx), n);
     cmplx *f0 = res;
     cmplx *f1 = f0 + np;
     cmplx *f2 = f1 + np;
     cmplx *f3 = f2 + np;
     cmplx *f4 = f3 + np;
-    for (size_t i = 0; i < np; i++) {
-        for (size_t j = 0; j < radix; j++) {
+    for (int i = 0; i < np; i++) {
+        for (int j = 0; j < radix; j++) {
             res[i + j * np] = input[radix * i + j];
         }
     }
@@ -123,18 +123,18 @@ void fft_radix5(const cmplx *input, cmplx *output, size_t n, int flag) {
     fft_radix5(f2, f2, np, flag);
     fft_radix5(f3, f3, np, flag);
     fft_radix5(f4, f4, np, flag);
-    float wexp0 = -2 * (float) M_PI * (flag) / (float) (n);
+    float wexp0 = -2 * M_PI * flag / n;
     cmplx wt = {cosf(wexp0), sinf(wexp0)};
     cmplx w0 = {1, 0};
-    for (size_t i = 0; i < np; i++) {
+    for (int i = 0; i < np; i++) {
         const float w0r = w0.real;
         w0.real = (w0r * wt.real) - (w0.imag * wt.imag);
         w0.imag = (w0.imag * wt.real) + (w0r * wt.imag);
     }
     cmplx w = {1, 0};
-    for (size_t j = 0; j < radix; j++) {
+    for (int j = 0; j < radix; j++) {
         cmplx wj = w;
-        for (size_t k = 0; k < np; k++) {
+        for (int k = 0; k < np; k++) {
             output[k + j * np] = cmplx_mul_add(f0[k], cmplx_mul_add(f1[k], cmplx_mul_add(f2[k],
                                                                                          cmplx_mul_add(f3[k], f4[k],
                                                                                                        wj), wj), wj),
@@ -150,13 +150,13 @@ void fft_radix5(const cmplx *input, cmplx *output, size_t n, int flag) {
     free(res);
 }
 
-void fft_radix6(const cmplx *input, cmplx *output, size_t n, int flag) {
+void fft_radix6(const cmplx *input, cmplx *output, int n, int flag) {
     if (n < 2) {
         output[0] = input[0];
         return;
     }
-    size_t radix = 6;
-    size_t np = n / radix;
+    int radix = 6;
+    int np = n / radix;
     cmplx *res = (cmplx *) calloc(sizeof(cmplx), n);
     cmplx *f0 = res;
     cmplx *f1 = f0 + np;
@@ -164,8 +164,8 @@ void fft_radix6(const cmplx *input, cmplx *output, size_t n, int flag) {
     cmplx *f3 = f2 + np;
     cmplx *f4 = f3 + np;
     cmplx *f5 = f4 + np;
-    for (size_t i = 0; i < np; i++) {
-        for (size_t j = 0; j < radix; j++) {
+    for (int i = 0; i < np; i++) {
+        for (int j = 0; j < radix; j++) {
             res[i + j * np] = input[radix * i + j];
         }
     }
@@ -175,18 +175,18 @@ void fft_radix6(const cmplx *input, cmplx *output, size_t n, int flag) {
     fft_radix6(f3, f3, np, flag);
     fft_radix6(f4, f4, np, flag);
     fft_radix6(f5, f5, np, flag);
-    float wexp0 = -2 * (float) M_PI * (flag) / (float) (n);
+    float wexp0 = -2 * M_PI * flag / n;
     cmplx wt = {cosf(wexp0), sinf(wexp0)};
     cmplx w0 = {1, 0};
-    for (size_t i = 0; i < np; i++) {
+    for (int i = 0; i < np; i++) {
         const float w0r = w0.real;
         w0.real = (w0r * wt.real) - (w0.imag * wt.imag);
         w0.imag = (w0.imag * wt.real) + (w0r * wt.imag);
     }
     cmplx w = {1, 0};
-    for (size_t j = 0; j < radix; j++) {
+    for (int j = 0; j < radix; j++) {
         cmplx wj = w;
-        for (size_t k = 0; k < np; k++) {
+        for (int k = 0; k < np; k++) {
             output[k + j * np] = cmplx_mul_add(f0[k], cmplx_mul_add(f1[k], cmplx_mul_add(f2[k],
                                                                                          cmplx_mul_add(f3[k],
                                                                                                        cmplx_mul_add(
@@ -205,13 +205,13 @@ void fft_radix6(const cmplx *input, cmplx *output, size_t n, int flag) {
     free(res);
 }
 
-void fft_radix7(const cmplx *input, cmplx *output, size_t n, int flag) {
+void fft_radix7(const cmplx *input, cmplx *output, int n, int flag) {
     if (n < 2) {
         output[0] = input[0];
         return;
     }
-    size_t radix = 7;
-    size_t np = n / radix;
+    int radix = 7;
+    int np = n / radix;
     cmplx *res = (cmplx *) calloc(sizeof(cmplx), n);
     cmplx *f0 = res;
     cmplx *f1 = f0 + np;
@@ -220,8 +220,8 @@ void fft_radix7(const cmplx *input, cmplx *output, size_t n, int flag) {
     cmplx *f4 = f3 + np;
     cmplx *f5 = f4 + np;
     cmplx *f6 = f5 + np;
-    for (size_t i = 0; i < np; i++) {
-        for (size_t j = 0; j < radix; j++) {
+    for (int i = 0; i < np; i++) {
+        for (int j = 0; j < radix; j++) {
             res[i + j * np] = input[radix * i + j];
         }
     }
@@ -232,18 +232,18 @@ void fft_radix7(const cmplx *input, cmplx *output, size_t n, int flag) {
     fft_radix7(f4, f4, np, flag);
     fft_radix7(f5, f5, np, flag);
     fft_radix7(f6, f6, np, flag);
-    float wexp0 = -2 * (float) M_PI * (flag) / (float) (n);
+    float wexp0 = -2 * M_PI * flag / n;
     cmplx wt = {cosf(wexp0), sinf(wexp0)};
     cmplx w0 = {1, 0};
-    for (size_t i = 0; i < np; i++) {
+    for (int i = 0; i < np; i++) {
         const float w0r = w0.real;
         w0.real = (w0r * wt.real) - (w0.imag * wt.imag);
         w0.imag = (w0.imag * wt.real) + (w0r * wt.imag);
     }
     cmplx w = {1, 0};
-    for (size_t j = 0; j < radix; j++) {
+    for (int j = 0; j < radix; j++) {
         cmplx wj = w;
-        for (size_t k = 0; k < np; k++) {
+        for (int k = 0; k < np; k++) {
             output[k + j * np] = cmplx_mul_add(f0[k], cmplx_mul_add(f1[k], cmplx_mul_add(f2[k],
                                                                                          cmplx_mul_add(f3[k],
                                                                                                        cmplx_mul_add(
@@ -265,8 +265,8 @@ void fft_radix7(const cmplx *input, cmplx *output, size_t n, int flag) {
     free(res);
 }
 
-void fft_Bluestein(const cmplx *input, cmplx *output, size_t n, int flag) {
-    size_t m = 1 << ((unsigned int) (ilogbf((float) (2 * n - 1))));
+void fft_Bluestein(const cmplx *input, cmplx *output, int n, int flag) {
+    int m = 1 << ((unsigned int) (ilogbf((float) (2 * n - 1))));
     if (m < 2 * n - 1) {
         m <<= 1;
     }
@@ -275,13 +275,12 @@ void fft_Bluestein(const cmplx *input, cmplx *output, size_t n, int flag) {
         return;
     cmplx *w = y + m;
     cmplx *ww = w + m;
-    float a0 = (float) M_PI / n;
     w[0].real = 1;
     if (flag == -1) {
         y[0].real = input[0].real;
         y[0].imag = -input[0].imag;
-        for (size_t i = 1; i < n; i++) {
-            const float wexp = a0 * i * i;
+        for (int i = 1; i < n; i++) {
+            const float wexp = M_PI * i * i / n;
             w[i].real = cosf(wexp);
             w[i].imag = sinf(wexp);
             w[m - i] = w[i];
@@ -291,8 +290,8 @@ void fft_Bluestein(const cmplx *input, cmplx *output, size_t n, int flag) {
     } else {
         y[0].real = input[0].real;
         y[0].imag = input[0].imag;
-        for (size_t i = 1; i < n; i++) {
-            const float wexp = a0 * i * i;
+        for (int i = 1; i < n; i++) {
+            const float wexp = M_PI * i * i / n;
             w[i].real = cosf(wexp);
             w[i].imag = sinf(wexp);
             w[m - i] = w[i];
@@ -302,34 +301,33 @@ void fft_Bluestein(const cmplx *input, cmplx *output, size_t n, int flag) {
     }
     fft_Stockham(y, y, m, 1);
     fft_Stockham(w, ww, m, 1);
-    for (size_t i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
         const float r = y[i].real;
         y[i].real = (r * ww[i].real) - (y[i].imag * ww[i].imag);
         y[i].imag = (y[i].imag * ww[i].real) + (r * ww[i].imag);
     }
     fft_Stockham(y, y, m, -1);
-    float scale = 1.0f / m;
     if (flag == -1) {
-        for (size_t i = 0; i < n; i++) {
-            output[i].real = ((y[i].real * w[i].real) + (y[i].imag * w[i].imag)) * scale;
-            output[i].imag = -((y[i].imag * w[i].real) - (y[i].real * w[i].imag)) * scale;
+        for (int i = 0; i < n; i++) {
+            output[i].real = ((y[i].real * w[i].real) + (y[i].imag * w[i].imag)) / m;
+            output[i].imag = -((y[i].imag * w[i].real) - (y[i].real * w[i].imag)) / m;
         }
     } else {
-        for (size_t i = 0; i < n; i++) {
-            output[i].real = ((y[i].real * w[i].real) + (y[i].imag * w[i].imag)) * scale;
-            output[i].imag = ((y[i].imag * w[i].real) - (y[i].real * w[i].imag)) * scale;
+        for (int i = 0; i < n; i++) {
+            output[i].real = ((y[i].real * w[i].real) + (y[i].imag * w[i].imag)) / m;
+            output[i].imag = ((y[i].imag * w[i].real) - (y[i].real * w[i].imag)) / m;
         }
     }
     free(y);
 }
 
-size_t base(size_t n) {
-    size_t t = n & (n - 1);
+int base(int n) {
+    int t = n & (n - 1);
     if (t == 0) {
         return 2;
     }
-    for (size_t i = 3; i <= 7; i++) {
-        size_t n2 = n;
+    for (int i = 3; i <= 7; i++) {
+        int n2 = n;
         while (n2 % i == 0) {
             n2 /= i;
         }
@@ -340,12 +338,12 @@ size_t base(size_t n) {
     return n;
 }
 
-void FFT(const cmplx *input, cmplx *output, size_t n) {
+void FFT(const cmplx *input, cmplx *output, int n) {
     if (n < 2) {
         output[0] = input[0];
         return;
     }
-    size_t p = base(n);
+    int p = base(n);
     switch (p) {
         case 2:
             fft_Stockham(input, output, n, 1);
@@ -368,12 +366,12 @@ void FFT(const cmplx *input, cmplx *output, size_t n) {
     }
 }
 
-void IFFT(const cmplx *input, cmplx *output, size_t n) {
+void IFFT(const cmplx *input, cmplx *output, int n) {
     if (n < 2) {
         output[0] = input[0];
         return;
     }
-    size_t p = base(n);
+    int p = base(n);
     switch (p) {
         case 2:
             fft_Stockham(input, output, n, -1);
@@ -395,10 +393,9 @@ void IFFT(const cmplx *input, cmplx *output, size_t n) {
             break;
         }
     }
-    float scale = 1.0f / n;
-    for (size_t i = 0; i < n; i++) {
-        output[i].real = output[i].real * scale;
-        output[i].imag = output[i].imag * scale;
+    for (int i = 0; i < n; i++) {
+        output[i].real = output[i].real / n;
+        output[i].imag = output[i].imag / n;
     }
 }
 
@@ -409,11 +406,11 @@ int main() {
     size_t N = 513;
     cmplx *input = (cmplx *) calloc(sizeof(cmplx), N);
     cmplx *output = (cmplx *) calloc(sizeof(cmplx), N);
-    for (size_t i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         input[i].real = i;
         input[i].imag = 0;
     }
-    for (size_t i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         printf("(%f %f) \t", input[i].real, input[i].imag);
     }
     for (int i = 0; i < 100; i++) {
@@ -421,7 +418,7 @@ int main() {
     }
     printf("\n");
     IFFT(output, input, N);
-    for (size_t i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         printf("(%f %f) \t", input[i].real, input[i].imag);
     }
     free(input);
